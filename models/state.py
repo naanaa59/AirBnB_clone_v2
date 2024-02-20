4d2cb5ab8a20823585b2ad539df368aa8f575ee1
Badr-annabi <badrannabi@gmail.com>
2024-02-20 16:35:30 +0000

    pycodestyle

--- models/state.py
+++ models/state.py
@@ -11,7 +11,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete-orphan", back_populates="state")
+    cities = relationship(
+        "City", cascade="all, delete-orphan", back_populates="state")
 
     @property
     def cities(self):
@@ -19,7 +20,7 @@ class State(BaseModel, Base):
         returns the list of City instances with state_id
         equals to the current State.id
         """
-        Session =sessionmaker(bind=Base.metadata.bind)
+        Session = sessionmaker(bind=Base.metadata.bind)
         with Session() as session:
             cities = session.query(City).filter(City.state_id == self.id).all()
         return cities
