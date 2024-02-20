48c5545812492035eb6da13c3bb82faf838726bb
oumaima <oumaimanaanaapro@gmail.com>
2024-02-20 22:28:16 +0100

    task7 and 8

--- models/place.py
+++ models/place.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
+
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from models.base_model import BaseModel
+from models.base_model import Base
+from models.user import User
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
+    """
     city_id = ""
     user_id = ""
     name = ""
@@ -16,3 +21,15 @@ class Place(BaseModel):
     latitude = 0.0
     longitude = 0.0
     amenity_ids = []
+    """
+    __tablename__ = "places"
+    city_id = Column(String(60), nullable=False, ForeignKey=('users.id'))
+    user_id = Column(String(60), nullable=False, ForeignKey=('users.id'))
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=False)
+    number_rooms = Column(Integer, default=0, nullable=False)
+    number_bathrooms = Column(Integer, default=0, nullable=False)
+    max_guest = Column(Integer, default=0, nullable=False)
+    price_by_night = Column(Integer, default=0, nullable=False)
+    latitude = Column(Float, nullable=False)
+    longitude = Column(Float, nullable=False)
