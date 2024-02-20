9a7d2e39efb17cb2916474aca64e9a38646d069a
Badr-annabi <badrannabi@gmail.com>
2024-02-20 14:46:17 +0000

    TASK 5 add delete() update all()

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,16 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.__objects
+        else:
+            filtered_objs = {}
+            for key, obj in self.__objects.items():
+                if type(obj) == cls:
+                    filtered_objs[key] = obj
+            return filtered_objs
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +31,18 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+
+    def delete(self, obj=None):
+        """
+        delete obj from __objects if it’s inside
+        if obj is equal to None,
+        the method should not do anything
+        """
+        if (obj is not None):
+            key = obj.__class__.__name__ + '.' + str(obj.id)
+            if key in self.__objects:
+                del self.__objects[key]
+                self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
@@ -45,6 +64,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
