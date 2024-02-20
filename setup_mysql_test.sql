e794bd307506e369eb009c8296d96db203e4acd0
oumaima <oumaimanaanaapro@gmail.com>
2024-02-20 10:12:07 +0100

    task4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,8 @@
+-- Creates hbnb_dev_db database with a new user hbnb_dev in localhost
+-- The password of hbnb_dev is hbnb_dev_pwd  The use have all priveleges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+USE hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
