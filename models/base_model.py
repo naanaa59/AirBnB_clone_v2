8980df5bb0a39ac2fef5ec55568d6711853ed5f1
oumaima <oumaimanaanaapro@gmail.com>
2024-02-21 09:54:36 +0100

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -11,24 +11,27 @@ Base = declarative_base()
 class BaseModel:
     __abstract__ = True
     """A base class for all hbnb models"""
-    id = Column(String(60), nullable=False, primary_key=True)
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
     created_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
     updated_at = Column(DateTime, default=datetime.utcnow(), nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            pass
+            self.id = str(uuid.uuid4())
+            self.created_at = self.updated_at = datetime.now()
         else:
             for key, value in kwargs.items():
-                if not hasattr(self, key):
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
+            if "id" not in kwargs:
+                self.id = str(uuid.uuid4())
+            if "created_at" not in kwargs:
+                self.created_at = datetime.now()
+            if "updated_at" not in kwargs:
+                self.updated_at = datetime.now()
     """
     def __init__(self, *args, **kwargs):
         Instatntiates a new model
