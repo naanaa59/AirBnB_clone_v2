25bb2c50dc664b2b0acf7481e4651ffd60fe7703
Badr-annabi <badrannabi@gmail.com>
2024-02-21 21:36:17 +0000

    Tests amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-""" """
+""" Tests Amenity model"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
+import os
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Tests Amenity model"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Initialized the tests for Amenity model """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ Tests the type of name  """
         new = self.value()
-        self.assertEqual(type(new.name), str)
+        self.assertNotEqual(
+            type(new.name),
+            str if os.getenv('HBNB_TYPE_STORAGE') != 'db' else type(None))
