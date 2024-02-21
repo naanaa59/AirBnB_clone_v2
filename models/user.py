b3b19ae9906b2e01935d2ccfa39d0c905165e389
oumaima <oumaimanaanaapro@gmail.com>
2024-02-21 21:20:27 +0100

    task78910

--- models/user.py
+++ models/user.py
@@ -4,6 +4,8 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import Place
+from models.review import Review
 
 
 class User(BaseModel, Base):
@@ -18,8 +20,8 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
     places = relationship('Place', backref="user",
                           cascade='all, delete, delete-orphan')
     reviews = relationship('Review', backref="user",
