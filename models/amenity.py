05a0258eefbc09367c1cf0102c5468a9d1013a9c
oumaima <oumaimanaanaapro@gmail.com>
2024-02-21 19:30:28 +0100

    models/

--- models/amenity.py
+++ models/amenity.py
@@ -5,9 +5,10 @@ from models.base_model import Base
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
+from models.place import Place
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary=place_amenity)
--- models/place.py
+++ models/place.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
