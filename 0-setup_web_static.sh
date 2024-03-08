4fe0edd6891f2e68dc343c5299a0bb0e38a45edc
oumaima <oumaimanaanaapro@gmail.com>
2024-03-08 01:12:15 +0100

    1 2 ouma

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
-# This scropt sets up the web servers for deployment
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -hR ubuntu:ubuntu /data/
-sudo sed -i '39i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
-sudo service nginx start
-=======
 # This script setes up web servers for the deployment of web_static
 
 # install nginx if it does'nt exist
@@ -73,4 +60,3 @@ if ! grep -q "$line" "$file"; then
 fi
 sudo service nginx restart
 
->>>>>>> 5d44770379199e442542e47558fa523c845c0e4e
