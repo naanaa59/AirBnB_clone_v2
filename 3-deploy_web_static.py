f5dfd4d49cab02ac5cb060313142670170aa65f5
oumaima <oumaimanaanaapro@gmail.com>
2024-03-10 23:10:51 +0000

    3 last try

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,78 +1,51 @@
 #!/usr/bin/python3
-"""
-    This is a Fabric script that generates a .tgz archive
-    from content of web_static folder
-    It deploys the archive to web servers
-"""
-from fabric.api import *
+"""Create and distributes an archive to web servers"""
+import os.path
 import time
-import os
+from fabric.api import local
+from fabric.operations import env, put, run
 
 env.hosts = ["54.158.203.28", "52.91.156.191"]
-env.user = "ubuntu"
-env.local_command_executed = False
 
 
 def do_pack():
-    """
-        adds all files in web_static folder to final archive
-        store them in a folder named versions
-        the name of the folder is:
-            web_static_<year><month><day><hour><minute><second>.tgz
-    """
-
+    """Generate an tgz archive from web_static folder"""
     try:
         local("mkdir -p versions")
-        source_path = "web_static/"
-        archive = "versions/web_static_{}.tgz".format(
-            time.strftime("%Y%m%d%H%M%S"))
-        local("tar -czvf {} {}".format(archive, source_path))
-
-        return archive
-
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".
+              format(time.strftime("%Y%m%d%H%M%S")))
+        return ("versions/web_static_{}.tgz".format(time.
+                                                    strftime("%Y%m%d%H%M%S")))
     except Exception as e:
         return None
 
 
 def do_deploy(archive_path):
-    """
-        Deploys and distributes an archive to web servers
-    """
-
+    """Distribute an archive to web servers"""
     if (os.path.isfile(archive_path) is False):
         return False
+
     try:
+        file = archive_path.split("/")[-1]
+        folder = ("/data/web_static/releases/" + file.split(".")[0])
         put(archive_path, "/tmp/")
-        archive_file = archive_path[9:]
-        server_archive = "/tmp/{}".format(archive_file)
-
-        archive_base, ext = os.path.splitext(archive_file)
-        new_path = "/data/web_static/releases/{}".format(archive_base)
-
-        run("sudo mkdir -p {}".format(new_path))
-        run("sudo tar -zxf {} -C {}".format(server_archive, new_path))
-        run("sudo rm  {}".format(server_archive))
-        run("sudo mv {}/web_static/* {}".format(new_path, new_path))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_path))
-
-        # print("New version deployed!")
+        run("mkdir -p {}".format(folder))
+        run("tar -xzf /tmp/{} -C {}".format(file, folder))
+        run("rm /tmp/{}".format(file))
+        run("mv {}/web_static/* {}/".format(folder, folder))
+        run("rm -rf {}/web_static".format(folder))
+        run('rm -rf /data/web_static/current')
+        run("ln -s {} /data/web_static/current".format(folder))
+        print("Deployment done")
         return True
     except Exception as e:
         return False
 
 
 def deploy():
-    """
-        creates and distributes an archive to your web servers
-    """
-    global path
+    """Create and distributes an archive to web servers"""
     try:
-        if not env.local_command_executed:
-            archive_path = do_pack()
-            env.local_command_executed = True
-            path = archive_path
-    # print(archive_path)
+        path = do_pack()
         return do_deploy(path)
     except Exception as e:
         return False
