1ea3b2bbe2f0996d16f2d5150a9910ea0da950e1
oumaima <oumaimanaanaapro@gmail.com>
2024-03-10 23:44:05 +0000

    rm exc

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -41,25 +41,25 @@ def do_deploy(archive_path):
 
     if (os.path.isfile(archive_path) is False):
         return False
-    try:
-        put(archive_path, "/tmp/")
-        archive_file = archive_path[9:]
-        server_archive = "/tmp/{}".format(archive_file)
+    # try:
+    put(archive_path, "/tmp/")
+    archive_file = archive_path[9:]
+    server_archive = "/tmp/{}".format(archive_file)
 
-        archive_base, ext = os.path.splitext(archive_file)
-        new_path = "/data/web_static/releases/{}".format(archive_base)
+    archive_base, ext = os.path.splitext(archive_file)
+    new_path = "/data/web_static/releases/{}".format(archive_base)
 
-        run("sudo mkdir -p {}".format(new_path))
-        run("sudo tar -zxf {} -C {}".format(server_archive, new_path))
-        run("sudo rm  {}".format(server_archive))
-        run("sudo mv {}/web_static/* {}".format(new_path, new_path))
-        run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(new_path))
+    run("sudo mkdir -p {}".format(new_path))
+    run("sudo tar -zxf {} -C {}".format(server_archive, new_path))
+    run("sudo rm  {}".format(server_archive))
+    run("sudo mv {}/web_static/* {}".format(new_path, new_path))
+    run("sudo rm -rf /data/web_static/current")
+    run("sudo ln -s {} /data/web_static/current".format(new_path))
 
-        # print("New version deployed!")
-        return True
-    except Exception as e:
-        return False
+    # print("New version deployed!")
+    return True
+    #except Exception as e:
+        # return False
 
 
 def deploy():
@@ -67,12 +67,12 @@ def deploy():
         creates and distributes an archive to your web servers
     """
     global path
-    try:
-        if not env.local_command_executed:
-            archive_path = do_pack()
-            env.local_command_executed = True
-            path = archive_path
-    # print(archive_path)
-        return do_deploy(path)
-    except Exception as e:
-        return False
+    # try:
+    if not env.local_command_executed:
+        archive_path = do_pack()
+        env.local_command_executed = True
+        path = archive_path
+# print(archive_path)
+    return do_deploy(path)
+    # except Exception as e:
+        # return False
