b7f742755100cf93e98857bd457437947f4c6c1e
oumaima <oumaimanaanaapro@gmail.com>
2024-03-22 20:05:37 +0000

    pycode

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,12 +8,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
-
-
-
