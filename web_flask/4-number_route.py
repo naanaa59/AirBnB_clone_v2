f3ed36ffd77c2d0387290be531ddc1752c4d4d1a
oumaima <oumaimanaanaapro@gmail.com>
2024-03-22 22:25:54 +0000

    4

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+'''
+    This script starts a Flask web application
+'''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_text(text='is_cool'):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<n>")
+def number(n):
+    n = int(n)
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
