adfd4303d91da1d14018b84baa0a29e00f65731b
oumaima <oumaimanaanaapro@gmail.com>
2024-03-23 00:22:38 +0000

    5

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+'''
+    This script starts a Flask web application
+'''
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    text = text.replace("_", " ")
+    return f"C {text}"
+
+
+@app.route("/python/")
+@app.route("/python/<text>")
+def python_text(text='is_cool'):
+    text = text.replace("_", " ")
+    return f"Python {text}"
+
+
+@app.route("/number/<int:n>")
+def number(n):
+    return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    return render_template("5-number.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
