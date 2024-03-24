e3ea65cc4a0c982b306dd2adb617a815056373c7
oumaima <oumaimanaanaapro@gmail.com>
2024-03-24 23:32:24 +0000

    rmove if statement

--- models/state.py
+++ models/state.py
@@ -6,6 +6,7 @@ from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import sessionmaker, relationship
 import os
+import shlex
 
 
 class State(BaseModel, Base):
@@ -15,10 +16,9 @@ class State(BaseModel, Base):
     cities = relationship(
         "City", cascade="all, delete-orphan", backref="state")
 
-    if not os.getenv('HBNB_TYPE_STORAGE') == "db":
-        @property
-        def cities(self):
-            from models import storage
-            cities_list = [city for _, city in storage.all(City).items()
-                           if city.state_id == self.id]
-            return cities_list
+    @property
+    def cities(self):
+        from models import storage
+        cities_list = [city for _, city in storage.all(City).items()
+                       if city.state_id == self.id]
+        return cities_list
